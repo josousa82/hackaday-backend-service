a52fa1fd6e1a4496487064e8a1c5ee315cedfe14
sousajo <josesousa@rentalcars.com>
2020-08-17 10:50:06 +0100

    Initial commit

---
+++ src/main/java/com/mchackton/hackatonproject/HackatonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mchackton.hackatonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonProjectApplication.class, args);
+
+    }
+}
